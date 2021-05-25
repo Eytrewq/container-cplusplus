24f2b7a7687a621a7922ea29ec6aab73e96e5b30
ebiscara <ebiscara@student.42.fr>
2021-05-25 13:54:43 +0200

    Iterartor modif

--- include/map.hpp
+++ include/map.hpp
@@ -6,7 +6,7 @@
 /*   By: ebiscara <ebiscara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 20:04:43 by ebiscara          #+#    #+#             */
-/*   Updated: 2021/05/23 18:13:54 by ebiscara         ###   ########.fr       */
+/*   Updated: 2021/05/25 13:53:02 by ebiscara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ namespace ft
 	template < class Key, class T >
 	struct BSTNode
 	{
-		std::pair<const Key, T> data;
+		std::pair<Key, T> data;
 		BSTNode *left;
 		BSTNode *right;
 		BSTNode *parent;
@@ -30,37 +30,20 @@ namespace ft
 	};
 
 	template < class Key, class T >
-	class MapIterator: public std::iterator<std::random_access_iterator_tag, T>
+	class MapIterator: public std::iterator<std::bidirectional_iterator_tag, T>
 	{
 		public:
-			typedef std::pair<const Key, T> value_type;
-			typedef std::pair<const Key, T>& reference;
+			typedef std::pair<Key, T> value_type;
+			typedef std::pair<Key, T>& reference;
 			typedef const std::pair<const Key, T>& const_reference;
-			typedef std::pair<const Key, T>* pointer;
-			typedef const std::pair<const Key, T>* const_pointer;
-			typedef BSTNode<const Key, T>* m_node;
+			typedef BSTNode<Key, T>* pointer;
+			typedef const BSTNode<Key, T>* const_pointer;
 		protected:
-			m_node n;
-		public:
-			MapIterator() {};
-			explicit MapIterator(m_node _n): n(_n) {};
-			MapIterator(const MapIterator &other) { *this = other; };
-			virtual ~MapIterator() {};
-
-			MapIterator &operator=(const MapIterator &other) { this->n = other.n; return (*this); };
-
-			bool operator==(const MapIterator &other) const { return (this->n == other.n); };
-			bool operator!=(const MapIterator &other) const { return (this->n != other.n); };
-
-			reference operator*() { return (this->n->data); };
-			const_reference operator*() const { return (this->n->data); };
-
-			pointer operator->() { return (this->n->data); };
-			const_pointer operator->() const { return (this->n->data); };
-
-			MapIterator operator++(int) { MapIterator tmp(*this); this->operator++(); return (tmp); };
-			MapIterator &operator++() {
-				m_node tmp = n;
+			pointer n;
+		private:
+			pointer n_next(pointer n)
+			{
+				pointer tmp = n;
 				if (tmp->right == 0)
 				{
 					while (tmp->parent && tmp->parent->right == tmp)
@@ -75,9 +58,9 @@ namespace ft
 				}
 				return (tmp);
 			};
-			MapIterator operator--(int) { MapIterator tmp(*this); this->operator--(); return (tmp); };
-			MapIterator &operator--() {
-				m_node tmp = n;
+			pointer n_pred(pointer n)
+			{
+				pointer tmp = n;
 				if (tmp->left == 0)
 				{
 					while (tmp->parent && tmp->parent->left == tmp)
@@ -92,8 +75,35 @@ namespace ft
 				}
 				return (tmp);
 			};
+		public:
+			MapIterator() {};
+			explicit MapIterator(pointer _n): n(_n) {};
+			MapIterator(const MapIterator &other) { *this = other; };
+			virtual ~MapIterator() {};
 
-			m_node getNode() { return (n); };
+			MapIterator &operator=(const MapIterator &other) { this->n = other.n; return (*this); };
+
+			bool operator==(const MapIterator &other) const { return (this->n == other.n); };
+			bool operator!=(const MapIterator &other) const { return (this->n != other.n); };
+
+			value_type &operator*() { return (this->n->data); };
+			value_type *operator->() { return (&this->n->data); };
+
+			//value_type &operator*() const { return (this->n->data); }; // Const
+			//value_type *operator->() const { return (&this->n->data); };
+
+			MapIterator operator++(int) { MapIterator tmp(*this); this->operator++(); return (tmp); };
+			MapIterator &operator++() {
+				this->n = this->n_next(this->n);
+				return (*this);
+			};
+			MapIterator operator--(int) { MapIterator tmp(*this); this->operator--(); return (tmp); };
+			MapIterator &operator--() {
+				this->n = this->n_pred(this->n);
+				return (*this);
+			};
+
+			pointer getNode() { return (n); };
 	};
 
 	template<class MapIterator>
@@ -105,29 +115,12 @@ namespace ft
 			typedef typename MapIterator::const_pointer const_pointer;
 			typedef typename MapIterator::reference reference;
 			typedef typename MapIterator::const_reference const_reference;
-			typedef typename MapIterator::m_node m_node;
 		protected:
-			m_node n;
-		public:
-			map_reverse_iterator() {};
-			explicit map_reverse_iterator(m_node _n): n(_n) {};
-			map_reverse_iterator(const map_reverse_iterator &other) { *this = other; };
-			virtual ~map_reverse_iterator() {};
-
-			map_reverse_iterator &operator=(const map_reverse_iterator &other) { this->n = other.n; return (*this); };
-
-			bool operator==(const map_reverse_iterator &other) const { return (this->n == other.n); };
-			bool operator!=(const map_reverse_iterator &other) const { return (this->n != other.n); };
-
-			reference operator*() { return (this->n->data); };
-			const_reference operator*() const { return (this->n->data); };
-
-			pointer operator->() { return (this->n->data); };
-			const_pointer operator->() const { return (this->n->data); };
-
-			map_reverse_iterator operator++(int) { map_reverse_iterator tmp(*this); this->operator++(); return (tmp); }; // ?
-			map_reverse_iterator &operator++() {
-				m_node tmp = n;
+			pointer n;
+		private:
+			pointer n_next(pointer n)
+			{
+				pointer tmp = n;
 				if (tmp->right == 0)
 				{
 					while (tmp->parent && tmp->parent->right == tmp)
@@ -142,9 +135,9 @@ namespace ft
 				}
 				return (tmp);
 			};
-			map_reverse_iterator operator--(int) { map_reverse_iterator tmp(*this); this->operator--(); return (tmp); };
-			map_reverse_iterator &operator--() {
-				m_node tmp = n;
+			pointer n_pred(pointer n)
+			{
+				pointer tmp = n;
 				if (tmp->left == 0)
 				{
 					while (tmp->parent && tmp->parent->left == tmp)
@@ -159,8 +152,32 @@ namespace ft
 				}
 				return (tmp);
 			};
+		public:
+			map_reverse_iterator() {};
+			explicit map_reverse_iterator(pointer _n): n(_n) {};
+			map_reverse_iterator(const map_reverse_iterator &other) { *this = other; };
+			virtual ~map_reverse_iterator() {};
 
-			m_node getNode() { return (n); };
+			map_reverse_iterator &operator=(const map_reverse_iterator &other) { this->n = other.n; return (*this); };
+
+			bool operator==(const map_reverse_iterator &other) const { return (this->n == other.n); };
+			bool operator!=(const map_reverse_iterator &other) const { return (this->n != other.n); };
+
+			value_type &operator*() { return (this->n->data); };
+			value_type *operator->() { return (&this->n->data); };
+
+			map_reverse_iterator operator++(int) { map_reverse_iterator tmp(*this); this->operator++(); return (tmp); }; // ?
+			map_reverse_iterator &operator++() {
+				this->n = this->n_next(this->n);
+				return (*this);
+			};
+			map_reverse_iterator operator--(int) { map_reverse_iterator tmp(*this); this->operator--(); return (tmp); };
+			map_reverse_iterator &operator--() {
+				this->n = this->n_pred(this->n);
+				return (*this);
+			};
+
+			pointer getNode() { return (n); };
 	};
 
 	template < class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<std::pair<const Key,T> > >
@@ -170,12 +187,12 @@ namespace ft
 			typedef Key key_type;
 			typedef T mapped_type;
 			typedef std::pair<const key_type,mapped_type> value_type;
-			typedef value_type& reference;
-			typedef const value_type& const_reference;
-			typedef value_type* pointer;
-			typedef const value_type* const_pointer;
+			typedef T& reference;
+			typedef const T& const_reference;
+			typedef T* pointer;
+			typedef const T* const_pointer;
 			typedef ft::MapIterator<Key, T> iterator;
-			typedef ft::MapIterator<const Key, const T> const_iterator; //??
+			typedef ft::MapIterator<Key, T> const_iterator; //??
 			typedef ft::map_reverse_iterator<iterator> reverse_iterator;
 			typedef ft::map_reverse_iterator<const_iterator> const_reverse_iterator;
 			typedef std::ptrdiff_t difference_type;
@@ -211,45 +228,54 @@ namespace ft
 				return (n);
 			};
 
-			_node _insert_node(_node n, key_type key, mapped_type val, bool end = false)
+			_node _insert_node(_node n, key_type key, mapped_type value, bool end = false)
 			{
-				while (n->end || (key < n->data.first && !end))
+				if (n->end)
 				{
 					if (!n->left)
 					{
-						n->left = create_node(key, val, n, end);
+						n->left = create_node(key, value, n, end);
 						return (n->left);
 					}
-					n = n->left;
+					return (_insert_node(n->left, key, value));
 				}
-				while (key >= n->data.first || end)
+				if (key < n->data.first && !end)
+				{
+					if (!n->left)
+					{
+						n->left = create_node(key, value, n, end);
+						return (n->left);
+					}
+					else
+						return (_insert_node(n->left, key, value));
+				}
+				else
 				{
 					if (!n->right)
 					{
-						n->right = create_node(key, val, n, end);
+						n->right = create_node(key, value, n, end);
 						return (n->right);
 					}
-					n = n->right;
+					else
+						return(_insert_node(n->right, key, value));
 				}
 			};
 
 			// ATT
 			_node _find(_node n, key_type key) const
 			{
-				_node tmp = n;
+				_node tmp;
 				if (!n->end && n->data.first == key && n->parent)
 					return (n);
-				while (tmp->right)
+				if (n->right)
 				{
-					if (!tmp->end && tmp->data.first == key && tmp->parent)
+					if ((tmp = _find(n->right, key)))
 						return (tmp);
-					n = n->right;
 				}
-				while (n->left)
+				if (n->left)
 				{
-					if (!n->end && n->data.first == key && n->parent)
-						return (n);
-					n = n->left;
+					if ((tmp = _find(n->left, key)))
+						return (tmp);
 				}
 				return (0);
 			};
@@ -257,9 +283,9 @@ namespace ft
 			void free_tree(_node n)
 			{
 				if (n->right)
-					free_tree(n->right)
+					free_tree(n->right);
 				if (n->left)
-					free_tree(n->left)
+					free_tree(n->left);
 				delete n;
 			};
 
